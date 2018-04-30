84bce287920c61e7712239ad53cb7c47b5b43a28
ps1011 <prasun4nepal@gmail.com>
2018-04-30 15:33:46 +0545

    Linked List

---
+++ App.py
@@ -0,0 +1,10 @@
+from linkedlist.LinkedList import LinkedList
+
+
+linkedlist = LinkedList()
+linkedlist.insertStart(12)
+linkedlist.insertStart(25)
+linkedlist.insertStart(24)
+linkedlist.insertEnd(16)
+linkedlist.insertStart(13)
+linkedlist.traverseList()
